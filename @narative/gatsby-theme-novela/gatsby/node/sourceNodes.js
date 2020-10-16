1f567e6343b5c414eb67ebd871fba49690b4e834
Valentine Erokhin <saviorand@gmail.com>
2020-10-16 11:04:53 +0100

    fix: fix dates and prune

--- @narative/gatsby-theme-novela/gatsby/node/sourceNodes.js
+++ @narative/gatsby-theme-novela/gatsby/node/sourceNodes.js
@@ -6,7 +6,7 @@ module.exports = ({ actions }) => {
       title: String!
       date: Date! @dateformat
       author: String!
-      excerpt(pruneLength: Int = 140): String!
+      excerpt(pruneLength: Int = 100): String!
       body: String!
       hero: File @fileByRelativePath
       timeToRead: Int
